6c3d589c777d27bfdf18360344c0180367dc1013
Christ <chri@iwm.fraunhofer.de>
2022-07-11 09:32:26 +0200

    Added comments in Stiffness_Plot.py

--- HomoPy/Stiffness_Plot.py
+++ HomoPy/Stiffness_Plot.py
@@ -9,32 +9,72 @@ Created on Thu Apr 28 16:24:54 2022
 
 import numpy as np
 
-USEVOIGT = False
+USEVOIGT = True # if False -> normalized Voigt (Mandel)
 sin = np.sin
 cos = np.cos
 
 def diade(di, dj):
     '''
-    Return diadic product of two direction vectors
+    Return diadic product of two directional vectors.
+    
+    Parameters:
+        - di : ndarray of shape(3,)
+            Directional vector #1.
+        - dj : ndarray of shape(3,)
+            Directional vector #2.
+            
+    Returns:
+        - ... : ndarray of shape(3, 3)
+            Tensor of 2nd order in tensor notation.
     '''
     return np.einsum("i,j->ij", di, dj)
 
 def matrix2voigt(matrix):
     '''
-    Return the voigt notation of a symmetric 3x3 matrix
+    Return the Voigt notation of a tensor of 2nd order 
+    calculated from the regular tensor notation.
+    
+    Parameters:
+        - matrix : ndarray of shape(3, 3)
+            Tensor in regular tensor notation.
+            
+    Returns:
+        - ... : ndarray of shape(6, 1)
+            Tensor in Voigt notation.
     '''
     return np.array([matrix[0,0], matrix[1,1], matrix[2,2],
                      matrix[1,2], matrix[0,2], matrix[0,1]])
 
 def matrix2mandel(matrix):
     '''
-    Return the mandel notation of a symmetric 3x3 matrix
+    Return the normalized Voigt notation of a tensor of 2nd order 
+    calculated from the regular tensor notation.
+    
+    Parameters:
+        - matrix : ndarray of shape(3, 3)
+            Tensor in regular tensor notation.
+            
+    Returns:
+        - ... : ndarray of shape(6, 1)
+            Tensor in normalized Voigt notation.
     '''
     b = np.sqrt(2)
     return np.array([matrix[0,0], matrix[1,1], matrix[2,2],
                      b*matrix[1,2], b*matrix[0,2], b*matrix[0,1]])
 
 def matrixreduction(matrix):
+    '''
+    Return the reduced notation (Voigt or normalized Voigt) depending
+    on which one is ought to be used.
+
+    Parameters:
+        - matrix : ndarray of shape(3, 3)
+            Tensor in regular tensor notation.
+    
+    Returns:
+        - ... : ndarray of shape(6,19
+            Tensor in Voigt or normalized Voigt notation.
+    '''
     if USEVOIGT:
         return matrix2voigt(matrix)
     else:
@@ -42,25 +82,76 @@ def matrixreduction(matrix):
 
 def get_reciprocal_E(didi, S):
     '''
-    Return the reciprocal of Young's modulus
+    Return the reciprocal of Young's modulus (compliance) in
+    the direction of di.
+
+    Parameters:
+        - didi : ndarray of shape(6, 1)
+            Directional tensor.
+        - S : ndarray of shape(6, 6)
+            Compliance tensor in Voigt or normalized Voigt
+            notation.
+
+    Returns:
+        - ... : float
+            Scalar compliance value in direction of di.
     '''
     return np.einsum('i,ij,j->', didi, S, didi)
 
 def get_E(di, S):
     '''
-    Return Young's modulus
+    Return Young's modulus in the direction of di.
+
+    Parameters:
+        - di : ndarray of shape(3, 1)
+            Directional vector.
+        - S : ndarray of shape(6, 6)
+            Compliance tensor in Voigt or normalized Voigt
+            notation.
+
+    Returns:
+        - ... : float
+            Scalar stiffness value in direction of didi.
     '''
     didi = matrixreduction(diade(di, di))
     return get_reciprocal_E(didi, S)**(-1)
 
 def dir_vec(theta, phi):
     '''
-    Return direction vector (parameterized with angles theta and phi)
+    Return directional vector based on angular parametrization.
+
+    Parameters:
+        - theta : float
+            First angle.
+        - phi : float
+            Second angle.
+
+    Returns:
+        - ... : ndarray of shape(3,)
+            Directional vector.
     '''
     return np.array([cos(theta)*sin(phi), sin(theta)*sin(phi), cos(phi)])
 
 
 def plot_E_body(S, o, p, bound=[0,0,0]):
+    '''
+    Plot stiffness body.
+
+    Parameters:
+        - S : ndarray of shape(6, 6)
+            Compliance tensor in Voigt or normalized Voigt
+            notation.
+        - o : int
+            Number of discretization steps for first angle.
+        - p : int
+            Number of discretization steps for second angle.
+        - bound : array of shape(3,), default=[0,0,0]
+            Boundaries for the 3 axis for the visualization.
+            If [0,0,0], boundaries are set automatically.
+
+    Returns:
+        - None
+    '''
     n = int(o); m = int(p)
     E_x = np.zeros((n+1, m+1))
     E_y = np.zeros((n+1, m+1))
@@ -103,6 +194,31 @@ def plot_E_body(S, o, p, bound=[0,0,0]):
     plt.show()
     
 def polar_plot_E_body(S, o, angle, bound=[0,0,0], plot=True):
+    '''
+    Plot slice of stiffness body.
+
+    Parameters:
+        - S : ndarray of shape(6, 6)
+            Compliance tensor in Voigt or normalized Voigt
+            notation.
+        - o : int
+            Number of discretization steps for first angle.
+        - angle : float
+            Angle to determine the angular orientation of the slice.
+            Does not work yet.
+        - bound : array of shape(3,), default=[0,0,0]
+            Boundaries for the 3 axis for the visualization.
+            If [0,0,0], boundaries are set automatically.
+        - plot : boolean
+            Determines whether the plot will be displayed. If False,
+            the metadata of the plot will be returned instead.
+
+    Returns:
+        - rad : ndarray of shape(n+1,)
+            Angular positions for polar plot.
+        - E : ndarray of shape(n+1,)
+            Sitffness at corresponding angle.
+    '''
     n = int(o)
     E = np.zeros(n+1)
     rad = np.zeros(n+1)
@@ -130,6 +246,18 @@ def polar_plot_E_body(S, o, angle, bound=[0,0,0], plot=True):
         return rad, E
     
 def polar_plot(data):
+    '''
+    Polar plot of multiple Stiffness bodies in one plot. For this
+    use the data generated from polar_plot_E_body with plot=False.
+
+    Parameters:
+        - data: list
+            Data to be plotted with angluar position, stiffness and
+            an optional string for the label in the plot.
+
+    Returns:
+        - None
+    '''
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
     for datum in data:
@@ -145,3 +273,45 @@ def polar_plot(data):
     
 
     
+if __name__=="__main__":
+
+    # Example
+    E = 3
+    nu = 0.35
+    K = E/(3*(1-2*nu))
+    G = E/(2*(1+nu))
+    diag = K + 4/3*G
+    offdiag = K - 2/3*G
+    stiffness1 = np.array([[diag, offdiag, offdiag, 0, 0, 0],
+    [offdiag, diag, offdiag, 0, 0, 0],
+    [offdiag, offdiag, diag, 0, 0, 0],
+    [0, 0, 0, G, 0, 0],
+    [0, 0, 0, 0, G, 0],
+    [0, 0, 0, 0, 0, G]])
+    compliance1 = np.linalg.inv(stiffness1)
+
+    E = 6
+    nu = 0.35
+    K = E/(3*(1-2*nu))
+    G = E/(2*(1+nu))
+    diag = K + 4/3*G
+    offdiag = K - 2/3*G
+    stiffness2 = np.array([[diag, offdiag, offdiag, 0, 0, 0],
+    [offdiag, diag, offdiag, 0, 0, 0],
+    [offdiag, offdiag, diag, 0, 0, 0],
+    [0, 0, 0, G, 0, 0],
+    [0, 0, 0, 0, G, 0],
+    [0, 0, 0, 0, 0, G]])
+    compliance2 = np.linalg.inv(stiffness2)
+
+    # Plot stiffness1 3D
+    plot_E_body(compliance1, 100, 100,)
+
+    # Polar plot stiffness1 directly
+    polar_plot_E_body(compliance1, 100, 0)
+
+    # Polar plot multiple stiffness
+    p1 = polar_plot_E_body(compliance1, 100, 0, plot=False)
+    p2 = polar_plot_E_body(compliance2, 100, 0, plot=False)
+
+    polar_plot([p1+('Stiffness1',), p2+('Stiffness2',)]) # here the commas in paranthesis are important
