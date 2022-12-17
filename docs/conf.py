273635b958c417acb7e970aa3bf6035d4bec126b
Nicolas Christ <christ.nicolas@outlook.de>
2022-12-17 21:13:18 +0100

    Updated readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'HomoPy'
-copyright = '2022, Nicolas Christ'
-author = 'Nicolas Christ'
+project = "HomoPy"
+copyright = "2022, Nicolas Christ"
+author = "Nicolas Christ"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_rtd_theme",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
