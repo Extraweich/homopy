ffc782598125ba065e54563f8cb0c86250ee898b
Nicolas Christ <christ.nicolas@outlook.de>
2023-01-19 15:58:16 +0100

    Added tests

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+from homopy.tensor import *
 
-def test_simple():
-    assert 1 == 1
+
+def test_orthogonal_base():
+    t = Tensor()
+
+    orthogonal_base = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    tensor_base = np.vstack((t.e1, t.e2, t.e3))
+
+    assert np.allclose(orthogonal_base, tensor_base, rtol=1e-6)
+
+
+def test_converter():
+    t = Tensor()
+
+    random_mat = np.random.rand(6, 6)
+    random_mat_sym = 1 / 2 * (random_mat + random_mat.T)
+
+    random_tensor_sym = t.mandel2tensor(random_mat_sym)
+    recovered_mat = t.tensor2mandel(random_tensor_sym)
+
+    assert np.allclose(random_mat_sym, recovered_mat, rtol=1e-6)
