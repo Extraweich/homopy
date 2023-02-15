eff79810f367ab743f405c22ce0719f21e497695
Nicolas Christ <nicolasc@hotmail.de>
2023-02-15 22:06:09 +0100

    v 1.0.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="homopy",
-    version="1.0.5",
+    version="1.0.6",
     author="Nicolas Christ",
     author_email="nicolas.christ@kit.edu",
     description="Your solution for stiffness problems",
